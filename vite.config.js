fad9daab67d7f167e47a143dc94cebc4789afa4d
Rafael Hengles <rhengles@gmail.com>
2026-05-25 22:12:44 -0300

    Add Vite build

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { dirname, resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, 'core/main.js'),
+      name: 'TransferSpeedGraph',
+      // the proper extensions will be added
+      fileName: 'arijs-transfer-speed-graph',
+    },
+    minify: 'terser',
+    outDir: 'core/dist',
+    // rolldownOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: [],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: 'Vue',
+    //     },
+    //   },
+    // },
+  },
+})
